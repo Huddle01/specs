014550ee572756145628ad79e1dc8c72d278dc1e
Om Gupta <omgupta0720@gmail.com>
2024-07-01 13:00:03 +0530

    fix: routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
+
+  redirect('/specs');
+
   return (
     <main className="flex h-screen flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
       <p className="text-muted-foreground">
         You can open{' '}
         <Link href="/docs" className="text-foreground font-semibold underline">
-          /docs
+          /specs
         </Link>{' '}
         and see the documentation.
       </p>
     </main>
   );
 }
+
