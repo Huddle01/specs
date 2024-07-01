8ce11975af318f18ba25ef640fb3e9f7d0c2c6a7
Arush Kurundodi <arushkurundodi@gmail.com>
2024-07-01 13:57:10 +0530

    add medianode rpc for registry

--- app/source.ts
+++ app/source.ts
@@ -3,7 +3,7 @@ import { loader } from 'fumadocs-core/source';
 import { createMDXSource } from 'fumadocs-mdx';
 
 export const { getPage, getPages, pageTree } = loader({
-  baseUrl: '/spcs',
+  baseUrl: '/specs',
   rootDir: 'specs',
   source: createMDXSource(map),
 });
