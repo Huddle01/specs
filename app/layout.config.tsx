aad38b0d0a97d979cbed9c2fa9e6eafbde28ba60
Om Gupta <omgupta0720@gmail.com>
2024-07-03 15:46:03 +0530

    fix: sidebar

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,10 +1,28 @@
 import { pageTree } from '@/app/source';
 import type { BaseLayoutProps, DocsLayoutProps } from 'fumadocs-ui/layout';
+import Image from 'next/image';
+import Logo from '@/public/logo.png';
 
 // shared configuration
 export const baseOptions: BaseLayoutProps = {
   nav: {
-    title: 'Huddle01 Specs',
+    // title: 'Huddle01 Specs',
+    enableSearch: true,
+    url: '/specs',
+    title: (
+        <>
+          <Image
+            alt="Fumadocs"
+            src={Logo}
+            sizes="100px"
+            className="hidden w-20 md:w-24 [.uwu_&]:block"
+            aria-label="Fumadocs"
+          />
+          <span className="font-medium [.uwu_&]:hidden max-md:[header_&]:hidden">
+            Fumadocs
+          </span>
+        </>
+    )
   },
 };
 
