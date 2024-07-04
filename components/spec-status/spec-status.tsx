8dcb3b7d8164f9b3d7d7a7b68bad8db89579f513
Arush Kurundodi <arushkurundodi@gmail.com>
2024-07-04 16:59:46 +0530

    fix color

--- components/spec-status/spec-status.tsx
+++ components/spec-status/spec-status.tsx
@@ -163,13 +163,13 @@ const SpecStatus = () => {
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="">
           <tr>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-left text-xs font-medium text-white-500 uppercase tracking-wider">
               Section
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-left text-xs font-medium text-white-500 uppercase tracking-wider">
               State
             </th>
-            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+            <th className="px-6 py-3 text-left text-xs font-medium text-white-500 uppercase tracking-wider">
               Theory Audit
             </th>
           </tr>
